5154509a98e734afe96b95ad9e4510b7e8da3305
SorerBOT <alonfiller@gmail.com>
2024-03-02 16:47:36 +0200

    (Feat) props are now passed by address

--- ex_4.c
+++ ex_4.c
@@ -49,19 +49,25 @@ Board initialiseBoard(int argc, char** argv);
 /**
  * Runs the game
 */
-void playGame(Board board);
+void playGame(Board* board);
 /**
  * Prints the initial state of the board
  * 
  * @return {void}
 */
+/**
+ * Plays one turn of the game
+ * 
+ * @return {void}
+*/
+void playTurn(Board* board, Player player);
 void printInitialBoard(Board board);
 /**
  * Prints the current state of the board
  * 
  * @return {void}
 */
-void printBoardState(Board board);
+void printBoardState(Board* board);
 /**
  * Checks if there are objects which have not yet been found
  * 
@@ -71,7 +77,7 @@ bool handleCheckIsGameFinished(Board board);
 /**
  * Finds the object located in the nth position
 */
-Object handleGetObjectAtPosition(Board board, int n);
+Object* handleGetObjectAtPosition(Board* board, int n);
 /**
  * Initialises the board using initialiseBoard
  * Prints the board using printInitialBoard
@@ -81,7 +87,7 @@ Object handleGetObjectAtPosition(Board board, int n);
 int main(int argc, char** argv) {
     Board board = initialiseBoard( argc, argv );
     if ( !board.isSuccess ) return 0;
-    playGame(board);
+    playGame( &board );
     return 0;
 }
 
@@ -165,13 +171,61 @@ Board initialiseBoard(int argc, char** argv) {
     board.isSuccess = true;
     return board;
 }
-void playGame( Board board ) {
+void playGame( Board* board ) {
     int playerIndex = 0;
-    printInitialBoard( board );
+    printInitialBoard( *board );
 
-    while ( !handleCheckIsGameFinished( board ) ) {
-        printBoardState(board);
-        return;
+    while ( !handleCheckIsGameFinished( *board ) ) {
+        printBoardState( board );
+        playTurn( board, ( *board ).players[playerIndex] );
+        playerIndex = ( ( playerIndex + 1 ) % ( *board ).numPlayers );
+    }
+}
+void playTurn(Board* board, Player player) {
+    int first_x, first_y;
+    int second_x, second_y;
+    Object* firstObject;
+    bool isFinishedTurn = false;
+    int boardLength = ( *board ).dim * ( *board ).dim;
+
+    printf( "It is %s's turn.\n", player.name );
+    
+    while ( !isFinishedTurn ) {
+        printf( "Enter the coordinates of the first card (row col): " );
+        scanf(" %d %d", &first_x, &first_y);
+
+        printf( "Enter the coordinates of the second card (row col): " );
+        scanf(" %d %d", &second_x, &second_y);
+        // Validate coordinates
+        if (
+            first_x < 0 ||
+            first_x >= ( *board ).dim ||
+            first_y < 0 ||
+            first_y >= ( *board ).dim ||
+            second_x < 0 ||
+            second_x >= ( *board ).dim ||
+            second_y < 0 ||
+            second_y >= ( *board ).dim ||
+            ( first_x == second_x && first_y == second_y )
+        ) {
+            printf( "Invalid move. Try again.\n" );
+            continue;
+        }
+        isFinishedTurn = true;
+        // Transform coordinates to index notation
+        int first_index = ( first_x * ( *board ).dim ) + first_y;
+        int second_index = ( second_x * ( *board ).dim ) + second_y;
+        // Get objects at said positions
+        firstObject = handleGetObjectAtPosition( board, first_index );
+        printf( "First pos: %d\nSecond pos: %d\n", ( *firstObject ).position.firstInstance, ( *firstObject ).position.secondInstance );
+        if ( ( *firstObject ).position.firstInstance == second_index || ( *firstObject ).position.secondInstance == second_index ) {
+            ( *firstObject ).isFound = true;
+            printf( "Match!\n" );
+            continue;
+        }
+        else {
+            printf( "No match. Try again.\n" );
+        }
     }
 }
 void printInitialBoard( Board board ) {
@@ -203,25 +257,28 @@ void printInitialBoard( Board board ) {
         printf( "%d. %s\n", i + 1, board.objects[i].name );
     }
 }
-void printBoardState( Board board ) {
-    int boardLength = ( board.dim * board.dim );
-    int dividerLength = ( 16 * board.dim ) + 1;
+void printBoardState( Board* board ) {
+    int boardLength = ( ( *board ).dim * ( *board ).dim );
+    int dividerLength = ( 16 * ( *board ).dim ) + 1;
     printf( "Current board state:\n" );
     for ( int i = 0; i < dividerLength; i++ ) printf( "-" );
     printf( "\n" );
     printf( "|" );        
     for ( int i = 0; i < boardLength; i++ ) {
-        Object object = handleGetObjectAtPosition( board, i );
-        int objectNameLength = strlen( object.name );
+        Object* object = handleGetObjectAtPosition( board, i );
+        // actualName will enable us to hide cards which are yet to have been found
+        char actualName[MAX_OBJECT_NAME_LENGTH];
+        strcpy( actualName, ( *object ).isFound ? ( *object ).name : "X" );
+        int objectNameLength = strlen( actualName );
         int amountOfSpacesNeeded = LENGTH_OF_CELL - objectNameLength;
         int amountOfSpacesNeededBefore = ( amountOfSpacesNeeded / 2 ) + ( amountOfSpacesNeededBefore % 2 );
         int amountOfSpacesNeededAfter = ( amountOfSpacesNeeded - amountOfSpacesNeededBefore );
 
         for ( int b = 0; b < amountOfSpacesNeededBefore; b++ ) printf(" ");
-        printf("%s", object.name);
+        printf("%s", actualName);
         for ( int b = 0; b < amountOfSpacesNeededAfter; b++ ) printf(" ");
         printf("|");
-        if ( !( ( i + 1 ) % board.dim ) ) {
+        if ( !( ( i + 1 ) % ( *board ).dim ) ) {
             printf( "\n" );
             for ( int b = 0; b < dividerLength; b++ ) printf( "-" );
             printf( "\n" );
@@ -235,8 +292,8 @@ bool handleCheckIsGameFinished(Board board) {
     }
     return true;
 }
-Object handleGetObjectAtPosition( Board board, int n ) {
-    for ( int i = 0; i < board.numObjects; i++ ) {
-        if ( board.objects[i].position.firstInstance == n || board.objects[i].position.secondInstance == n ) return board.objects[i];
+Object* handleGetObjectAtPosition( Board* board, int n ) {
+    for ( int i = 0; i < ( *board ).numObjects; i++ ) {
+        if ( ( *board ).objects[i].position.firstInstance == n || ( *board ).objects[i].position.secondInstance == n ) return &( ( * board ).objects[i] );
     }
 }
